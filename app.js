0a296d0a26dd95d37d541e13c00646ee30d471eb
Romain Devalle <31740752+Zoryi@users.noreply.github.com>
2025-06-09 20:11:10 +0200

    Update index.html (#1)
    * Update index.html
    * Update app.js
    * Update service-worker.js

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-// Vérifie si les Service Workers sont supportés par le navigateur
 if ('serviceWorker' in navigator) {
-    // Écoute l'événement 'load' de la fenêtre pour enregistrer le Service Worker
     window.addEventListener('load', () => {
-        // Enregistre le Service Worker situé à la racine du site
         navigator.serviceWorker.register('/service-worker.js')
             .then(registration => {
-                // Succès de l'enregistrement
                 console.log('Service Worker enregistré avec succès:', registration.scope);
             })
             .catch(error => {
-                // Échec de l'enregistrement
                 console.error('Échec de l\'enregistrement du Service Worker:', error);
             });
     });
